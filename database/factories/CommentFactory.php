553ab4307429b7f9545ba4fa5692920edf6fde5d
Nelson Otazo <nelson.otazo@gmail.com>
2023-07-06 16:34:26 -0400

    model, factory, migration and seeder for comments are created

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
+ */
+class CommentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user' => fake()->name(),
+            'message' => fake()->text(),
+            'level' => fake()->numberBetween(1, 3)
+        ];
+    }
+}
