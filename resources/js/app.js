8b77f24e7eb5d0513df97a8214667d7ba97a32f3
Nelson Otazo <nelson.otazo@gmail.com>
2023-07-06 15:25:43 -0400

    VueJS 3 installed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-import './bootstrap';
+import { createApp } from "vue/dist/vue.esm-bundler";
+import "./bootstrap";
+import Home from "./components/Home.vue";
+
+const app = createApp({});
+
+app.component("home", Home);
+
+app.mount("#app");
