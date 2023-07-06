cb5997e818b5b09175006c318d4f3d0d133b1e0d
Nelson Otazo <nelson.otazo@gmail.com>
2023-07-06 16:50:11 -0400

    the form to add comments is added

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -25,6 +25,24 @@ onMounted(() => {
               <h2 class="text-lg font-bold text-gray-900">Discussion</h2>
             </div>
 
+            <form>
+              <div class="py-2 px-4 mb-4 rounded-lg rounded-t-lg border border-gray-200">
+                <label for="user" class="sr-only">Your name</label>
+                <input class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none" id="user"
+                  type="text" placeholder="Your name...">
+              </div>
+              <div class="py-2 px-4 mb-4 rounded-lg rounded-t-lg border border-gray-200">
+                <label for="comment" class="sr-only">Your comment</label>
+                <textarea id="comment" rows="5"
+                  class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none"
+                  placeholder="Write a comment..." required></textarea>
+              </div>
+              <button type="submit"
+                class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-cyan-500 rounded-lg">
+                Post comment
+              </button>
+            </form>
+
             <article v-for="comment in comments" class="p-6 mb-4 text-base rounded-lg">
               <div class="flex justify-between items-center mb-2">
                 <div class="flex items-center">
