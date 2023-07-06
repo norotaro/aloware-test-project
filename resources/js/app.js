24d9e4dace5e4f1c65a2b69e52b1c210c369363c
Nelson Otazo <nelson.otazo@gmail.com>
2023-07-06 16:38:48 -0400

    Post body is moved to it's own component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,13 @@
-import { createApp } from "vue/dist/vue.esm-bundler";
-import Home from "./components/Home.vue";
 import "./bootstrap";
 import "../css/app.css";
+
+import { createApp } from "vue/dist/vue.esm-bundler";
+import Home from "./components/Home.vue";
+import Post from "./components/Post.vue";
 
 const app = createApp({});
 
 app.component("home", Home);
+app.component("post", Post);
 
 app.mount("#app");
--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -17,37 +17,7 @@ onMounted(() => {
   <main class="pt-8 pb-16">
     <div class="flex justify-between px-4 mx-auto">
       <article class="mx-auto w-full max-w-2xl">
-        <header class="mb-4">
-          <div class="flex items-center mb-4">
-            <div class="inline-flex items-center text-sm mr-3">
-              <img class="rounded-full w-16 h-16 mr-4" src="https://placeholder.co/50?text=avatar" alt="User">
-              <div>
-                <a href="#" class="text-xl font-bold text-gray-900">Nelson Otazo</a>
-                <p class="text-base font-light text-gray-500">Full-Stack developer</p>
-                <p class="text-base font-light text-gray-500">
-                  <time datetime="2023-07-06">July 6, 2023</time>
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <h1 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900">
-            Aloware test project
-          </h1>
-        </header>
-
-        <div>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi quo voluptatum incidunt aperiam sequi
-            quidem obcaecati vitae veniam corporis perferendis, debitis itaque omnis porro sapiente molestias aliquam et
-            voluptates. Praesentium?</p>
-          <figure>
-            <img class="p-5" src="https://placeholder.co/300?text=Blog+image" alt="blog image">
-          </figure>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus amet doloremque aut. Esse at repellendus,
-            deserunt quam eligendi sequi distinctio optio autem. Nisi corporis voluptates, libero quas rem vel a.
-          </p>
-        </div>
+        <post></post>
 
         <section class="py-8">
           <div class="max-w-2xl mx-auto px-4">
